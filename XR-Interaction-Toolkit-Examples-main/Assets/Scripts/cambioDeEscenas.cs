8b77498bca26c7375e94e1017f3c680078c50f83
Laura <lgomez25@ucm.es>
2023-11-21 15:19:28 +0100

    cambio de escenas y movimiento por teclado

---
+++ XR-Interaction-Toolkit-Examples-main/Assets/Scripts/cambioDeEscenas.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambioDeEscenas : MonoBehaviour
+{
+    public GameManager gm;
+    public string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("ay");
+            gm.changeScene(nextScene);
+        }
+    }
+}
