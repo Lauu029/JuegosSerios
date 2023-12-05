35b668ac86e88e2734ae73d032936418fa7c89df
Laura Gomez <lgomez25@ucm.es>
2023-12-05 14:48:52 +0100

    Mas objetos en la escena

--- MenteEnConflicto/Assets/Scripts/dontDestroyOnLoad.cs
+++ MenteEnConflicto/Assets/Scripts/dontDestroyOnLoad.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class DontDestroyOnLoad : MonoBehaviour
+public class dontDestroyOnLoad : MonoBehaviour
 {
     // Start is called before the first frame update
     void Awake()
