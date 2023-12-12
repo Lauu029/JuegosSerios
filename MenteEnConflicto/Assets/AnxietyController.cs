6b54a3f63a322e4ea3b506957914da511b711cce
Laura Gomez <lgomez25@ucm.es>
2023-12-13 00:42:31 +0100

    ai curasao

--- MenteEnConflicto/Assets/AnxietyController.cs
+++ MenteEnConflicto/Assets/AnxietyController.cs
@@ -1,18 +1,29 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnxietyController : MonoBehaviour
 {
+    private FMOD.Studio.EventInstance instance;
+
+    public FMODUnity.EventReference fmodEvent;
+    
+    [SerializeField]
+    [Range(0, 3)]
+    private int anxiety;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        anxiety = 0;
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        instance.setParameterByName("Anxiety", anxiety);
     }
 }
