f30d3cbc17f40cc3019f95451910621d09798b9d
Sergio Baña <sebana@ucm.es>
2023-12-12 18:50:23 +0100

    personas con ia y animacion

--- MenteEnConflicto/Assets/Scripts/IA/PathFollow.cs
+++ MenteEnConflicto/Assets/Scripts/IA/PathFollow.cs
@@ -8,16 +8,16 @@ public class PathFollow : MonoBehaviour
     private NavMeshAgent navMeshAgent;
 
     [SerializeField]
-    private float radius = 10.0f;
+    private float radius = 40.0f;
 
     void Start()
     {
-        navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        if ((!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f) || navMeshAgent.velocity.magnitude < 0.5f)
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
         {
             Vector3 randomDirection = Random.insideUnitSphere * radius + transform.position;
             navMeshAgent.SetDestination(randomDirection);
