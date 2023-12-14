568dd28aa112b7a09df69d7db1add5370f758417
pablosm196 <pablos19@ucm.es>
2023-12-14 15:51:04 +0100

    Cambios en sendas escenas

--- MenteEnConflicto/Assets/Scripts/keyboardMovement.cs
+++ MenteEnConflicto/Assets/Scripts/keyboardMovement.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class keyboardMovement : MonoBehaviour
 {
     public float velocidad = 0.5f;
+    private CharacterController _myCharacterController;
     // Start is called before the first frame update
     void Start()
     {
+        _myCharacterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Vertical");
+        float v = Input.GetAxisRaw("Horizontal");
 
         Vector3 movimiento = new Vector3(h, 0.0f, v);
         movimiento.Normalize();
         movimiento *= velocidad * Time.deltaTime;
 
-        transform.position += movimiento;
+        _myCharacterController.Move(movimiento);
     }
 }
